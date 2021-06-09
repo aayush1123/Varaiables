775395bb0531f2f628f0d61df6e06c8cbd58e05e
aayush pradhan <78127930+aayush1123@users.noreply.github.com>
2021-06-09 08:37:48 +0545

    Add files via upload

---
+++ a.py
@@ -0,0 +1,43 @@
+#string varaiable
+print("STRING VARAIABLE")
+a="1234skfj" #anything inside the double or single quotation mark is string and "a" is a string varaible
+print(a) 
+a,b="name",'address'#here the value of "a" is "name" and b="address"
+print(a)
+print(b)
+print(b+a)
+# a and b  cannot be mutiply but can be multipy by only integer varaible 
+print(a*2) 
+c="""hello
+hi
+how are you ?
+Are you fine?"""#by using triple quotation mark you can write the string in next line
+print(c)
+a="3"
+b="2"
+print(a+b)#3+2=5 but a and b is a string variable so like in 7th line its join
+
+
+# number varaible
+print("NUMBER VARAIBLE")
+x=2 #not inside quotation mark
+y=4.55
+print(x+y)#(the number will be added so x+y=6.55)
+#there are two types of number varaiable
+#1)Integer (without decimal value)like:
+x,y=2,3
+#2) float(Any number with decimal value)
+x,y=2.0,3.7#(2.0 is also  a float)
+print(x+y)
+
+
+#boolean varaiable
+print("BOOLEAN VARAIABLE") #means only True or False
+a=True #T or Fshould be capital
+b=False 
+print(a)
+x=2
+y=4
+z=2
+print(y==z)# '=' should be double for checking 
+print(x==z)#as the x and y=2 so result will be True
